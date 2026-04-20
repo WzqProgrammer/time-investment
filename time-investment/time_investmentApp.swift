e2199fed60863bf1ac509f8c303721f3e79bcea5
wzqStatic <1418807683@qq.com>
2026-04-20 23:07:40 +0800

    init project

---
+++ time-investment/time_investmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_investmentApp.swift
+//  time-investment
+//
+//  Created by wangzhengqing on 2026/4/20.
+//
+
+import SwiftUI
+
+@main
+struct time_investmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ time-investmentTests/time_investmentTests.swift
@@ -0,0 +1,18 @@
+//
+//  time_investmentTests.swift
+//  time-investmentTests
+//
+//  Created by wangzhengqing on 2026/4/20.
+//
+
+import Testing
+
+struct time_investmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
